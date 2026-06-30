58d43990b556b680b847e3f219cf59e5a94434f1
DREAM <fsx670331@qq.com>
2026-06-30 18:09:42 +0800

    add Capacitor Android + APK build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.epaper.companion',
+  appName: 'EPaper',
+  webDir: 'dist'
+};
+
+export default config;
